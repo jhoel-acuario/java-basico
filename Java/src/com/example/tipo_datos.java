e087334781508af7edd7cde47a45a0aa461e8038
desarrollador198 <desarrollador198_usi@minedu.gob.pe>
2023-02-22 15:34:14 -0500

    tipos de datos

---
+++ Java/src/com/example/tipo_datos.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class tipo_datos {
+    public static void main(String[] args) {
+        // 1. numericos
+
+
+        // 1.1 enteros
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        long numeroTelefono = 666555444;
+        // long precio = null;
+
+        // 1.2 decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+        variable6 = 20.5d;
+        double precioSilla = 49.99;
+
+        // 2. booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. texto
+        char variable9 = 'a';
+        String variable10 = "Lorem ipsum dolor sit amet.....";
+    }
+}
